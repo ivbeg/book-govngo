1366a09ce8bb92b5f8f2a3b3637fc9e1d4465a99
Ivan Begtin <ivan@begtin.tech>
2022-09-05 11:51:52 +0300

    deploy new book

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-git worktree add tmp/gh-pages
+git worktree add /tmp/gh-pages
 mdbook build
-rm -rf tmp/book/* # this won't delete the .git directory
-mkdir tmp/book
-cp -rp book/* tmp/book/
-cd tmp/book
+rm -rf /tmp/book/* # this won't delete the .git directory
+#mkdir tmp/book
+cp -rp book/* /tmp/book/
+cd /tmp/book
 git add -A
 git commit -m "deploy new book"
 git push origin gh-pages
